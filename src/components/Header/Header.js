8e0dc6fe0b4c70a8871a426571fc79121d6c132c
Alex1304 <mirandaa1304@gmail.com>
2018-09-11 00:06:20 +0200

    placeholder header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <header className="Header">
+        <nav className="Header-nav navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">MyGDashboard</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Header;
